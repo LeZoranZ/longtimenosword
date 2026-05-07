a8cb3dd312d3615a6b266076a5aabdf56428816b
LeZoranZ <lezoranz@hotmail.com>
2026-05-07 14:05:47 +0200

    Changed material stats for the hand guard to BINDING

--- src/main/java/me/creeperscrown/longtimenosword/item/TinkersItems.java
+++ src/main/java/me/creeperscrown/longtimenosword/item/TinkersItems.java
@@ -6,14 +6,14 @@ import slimeknights.mantle.registration.object.ItemObject;
 import slimeknights.tconstruct.common.registration.CastItemObject;
 import slimeknights.tconstruct.common.registration.ItemDeferredRegisterExtension;
 import slimeknights.tconstruct.library.tools.part.ToolPartItem;
-import slimeknights.tconstruct.tools.stats.GripMaterialStats;
+import slimeknights.tconstruct.tools.stats.StatlessMaterialStats;
 
 public class TinkersItems {
 
     public static final ItemDeferredRegisterExtension ITEMS = new ItemDeferredRegisterExtension(Longtimenosword.MODID);
 
     public static final ItemObject<ToolPartItem> HAND_GUARD = ITEMS.register("hand_guard",
-                    ()-> new ToolPartItem(Longtimenosword.defaultItemProperties(), GripMaterialStats.ID)); //StatlessMaterialStats.BINDING.getIdentifier()
+                    ()-> new ToolPartItem(Longtimenosword.defaultItemProperties(), StatlessMaterialStats.BINDING.getIdentifier())); //GripMaterialStats.ID
     public static final CastItemObject HAND_GUARD_CAST = ITEMS.registerCast("hand_guard", new Item.Properties());
     public static final ItemObject<Longsword> LONGSWORD = ITEMS.register("longsword", Longsword::new);
 
